22695aa37e77a068d5a22282cb490241d54637db
tony <tony@nautilusgames.com>
2024-07-04 00:35:52 +0700

    NAU-6217 structure api

--- auth/internal/handler/cors.go
+++ auth/internal/handler/cors.go
@@ -8,7 +8,6 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 
-		// Handle preflight requests
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
