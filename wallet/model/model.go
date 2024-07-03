00d08a02cb3c3e322fca26a46d5f9baeae081c56
Sol <sol@ugaming.io>
2024-07-04 00:56:24 +0700

    NAU-6218 update internal address

--- wallet/model/model.go
+++ wallet/model/model.go
@@ -1,7 +1,7 @@
 package model
 
 const (
-	InternalAddress  = "http://demo-wallet:8082"
+	InternalAddress  = "http://demo-wallet:8080"
 	CreateWalletPath = "/api/v1/wallet/create-wallet"
 	TransferPath     = "/api/v1/wallet/transfer"
 	GetWalletPath    = "/api/v1/wallet/get-wallet"
